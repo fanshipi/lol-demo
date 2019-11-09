80d0f04ee10cb5df7c81550cf1f4aa6bbfcbf874
fanshipi <545939644@qq.com>
2019-11-09 17:02:14 +0800

    url

--- config/index.js
+++ config/index.js
@@ -33,8 +33,8 @@ module.exports = {
     env: require('./prod.env'),
     index: path.resolve(__dirname, `../dist/${fileExt.platform}/index.html`),
     assetsRoot: path.resolve(__dirname, `../dist/${fileExt.platform}`),
-    assetsSubDirectory: '',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/dist',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
